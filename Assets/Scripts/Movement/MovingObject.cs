9c739a5d46f0ba1aeaee8462af232efafbb31d77
waagreen <silvacostawagner@gmail.com>
2022-09-22 20:30:33 -0300

    changing the way objects move

--- Assets/Scripts/Movement/MovingObject.cs
+++ Assets/Scripts/Movement/MovingObject.cs
@@ -9,19 +9,23 @@ public class MovingObject : PoolingObjectReturner
     private GlobalMovement globalMove;
     
     private float screenWidth => Screen.width;
+    private Vector3 myRightEdge;
+    private float screenLeftEdge;
     private Vector3 objectPosition => Camera.main.WorldToScreenPoint(transform.position + transform.localScale);
 
     private void Awake()
     {
         globalMove = GameObject.Find("-- MANAGER").GetComponent<GlobalMovement>();
+        myRightEdge = GetComponent<BoxCollider>().bounds.max;
+        screenLeftEdge = Camera.main.ScreenToWorldPoint(Vector2.zero).x;
     }
 
     private void FixedUpdate()
     {
         transform.position += (Vector3.left * globalMove.ActualSpeed) * Time.fixedDeltaTime;
 
-        float positionSquared = Mathf.Pow(objectPosition.x, 2f);
-        if(objectPosition.x < screenWidth - 4800) gameObject.SetActive(false);
+        var updatedEdge = myRightEdge.x + transform.position.x + 0.5f;
+        if(updatedEdge < screenLeftEdge) gameObject.SetActive(false);
     }
 
 }
