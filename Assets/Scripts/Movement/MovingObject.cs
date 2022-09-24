c6f39483f2123a0dd27c4b3238cdf16d3cc386fe
Gustavo Mondini <gmondini7@gmail.com>
2022-09-24 10:38:39 -0300

    Design do Lvl 1
    Design do level 1 faltando apenas os inimigos. Implementação da posição das moedas e das plataformas principais completa, semelhante ao modelo do lipe.

--- Assets/Scripts/Movement/MovingObject.cs
+++ Assets/Scripts/Movement/MovingObject.cs
@@ -21,7 +21,7 @@ public class MovingObject : PoolingObjectReturner
         transform.position += (Vector3.left * globalMove.ActualSpeed) * Time.fixedDeltaTime;
 
         float positionSquared = Mathf.Pow(objectPosition.x, 2f);
-        if(objectPosition.x < screenWidth - 4800) gameObject.SetActive(false);
+        if(objectPosition.x < screenWidth - 4800) gameObject.SetActive(false); 
     }
 
 }
