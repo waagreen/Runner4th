347f490495bfb093d13e2bc4a11a030590d56600
waagreen <silvacostawagner@gmail.com>
2022-09-26 20:48:49 -0300

    fixing the first level prefabs

--- Assets/Scripts/Movement/MovingObject.cs
+++ Assets/Scripts/Movement/MovingObject.cs
@@ -3,24 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using MyBox;
 
+[RequireComponent(typeof(BoxCollider))]
 public class MovingObject : PoolingObjectReturner
 {   
     private float speed => DataManager.GlobalMovement.ActualSpeed;
     
     private Camera principalCamera;
     private BoxCollider myExtension;
+    Vector3 leftBorder;
+    private Animator test;
 
     private void Awake() {
         if(principalCamera == null) principalCamera = Camera.main;
         if(myExtension == null) myExtension = GetComponent<BoxCollider>();
+        
+        float dist = (transform.position - principalCamera.transform.position).z;
+        leftBorder = principalCamera.ViewportToWorldPoint(new Vector3(0, 0, dist));
     }
 
     private void FixedUpdate()
     {
         transform.position += (Vector3.left * speed) * Time.fixedDeltaTime;
-        
-        float dist = (transform.position + transform.localScale - principalCamera.transform.position).z;
-        Vector3 leftBorder = principalCamera.ViewportToWorldPoint(new Vector3(0, 0, dist));
 
         if(transform.position.x < leftBorder.x - (myExtension.size.x + 2.5f)) gameObject.SetActive(false);
     }
