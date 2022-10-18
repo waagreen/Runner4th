acbe59d8d4ee4160bdc55dbccb840f37a86d7f12
Daniel Bisaggio <danniells.bisaggio@gmail.com>
2022-10-18 20:54:24 -0300

    Finding the formula of sin to apply in the obstacles

---
+++ Assets/Scripts/Enemies/EnemiePlataform.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemiePlataform : MonoBehaviour
+{
+
+    [SerializeField] private float amplification;
+    [SerializeField] private float frequency;
+    private Vector3 currentPosition;
+
+    void Awake() => currentPosition = transform.position;
+
+    void Update(){
+        transform.position = new Vector3(currentPosition.x, Mathf.Sin(Time.deltaTime * frequency) * amplification + currentPosition.y, currentPosition.z);
+    }
+    
+}
