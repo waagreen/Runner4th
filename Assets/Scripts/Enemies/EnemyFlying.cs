3f9657f17db17af9e472b40cecf92b91bf0659fb
Daniel Bisaggio <danniells.bisaggio@gmail.com>
2022-10-19 17:19:08 -0300

    Flying enemy moving and shooting

--- Assets/Scripts/Enemies/EnemyFlying.cs
+++ Assets/Scripts/Enemies/EnemyFlying.cs
@@ -6,7 +6,10 @@ public class EnemyFlying : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private float lineOfSite;
+    [SerializeField] private float shootingRange;
+    [SerializeField] private float fireRate = 1f;
     [SerializeField] private GameObject bullet, bulletParent;
+    private float fireTime;
     private Transform player;
 
     //find the player
@@ -17,9 +20,14 @@ public class EnemyFlying : MonoBehaviour
         //calculate the distance between player and enemie
         float distanceFromPlayer = Vector3.Distance(player.position, transform.position);
 
-        //set a limit to the enemie view
-        if(distanceFromPlayer < lineOfSite){
+        //set a limit to the enemie view and shooting range
+        if(distanceFromPlayer < lineOfSite && distanceFromPlayer > shootingRange){
             transform.position = Vector3.MoveTowards(this.transform.position, player.position, speed * Time.deltaTime);
+        }
+        //preparing to shoot and cooldown for it
+        else if(distanceFromPlayer <= shootingRange && fireTime < Time.time){
+            Instantiate(bullet, bulletParent.transform.position, Quaternion.identity);
+            fireTime = Time.time + fireRate;
         }
 
     }
@@ -28,5 +36,6 @@ public class EnemyFlying : MonoBehaviour
         Gizmos.color = Color.green;
         //set the trigger sphere
         Gizmos.DrawWireSphere(transform.position, lineOfSite);
+        Gizmos.DrawWireSphere(transform.position, shootingRange);
     }
 }
