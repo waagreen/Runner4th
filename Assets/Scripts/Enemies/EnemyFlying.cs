43f6e211db5f5a2fed129b8424a7ebdab8c4724e
Daniel Bisaggio <danniells.bisaggio@gmail.com>
2022-10-20 08:09:16 -0300

    adjustments

--- Assets/Scripts/Enemies/EnemyFlying.cs
+++ Assets/Scripts/Enemies/EnemyFlying.cs
@@ -4,16 +4,28 @@ using UnityEngine;
 
 public class EnemyFlying : MonoBehaviour
 {
+    [Header("Speed and Bounds")]
     [SerializeField] private float speed;
     [SerializeField] private float lineOfSite;
+    private Transform player, target;
+    
+    [Space]
+    [Header("Bullet Property")]
     [SerializeField] private float shootingRange;
     [SerializeField] private float fireRate = 1f;
     [SerializeField] private GameObject bullet, bulletParent;
     private float fireTime;
-    private Transform player;
+
+    [Space]
+    [Header("Time for Destroy then object")]
+    [SerializeField] private float timeToDie = 10f;
+    private float timeToDestroy;
 
     //find the player
-    void Awake() => player = GameObject.FindGameObjectWithTag(DataManager.playerTag).transform;
+    void Awake(){
+        player = GameObject.FindGameObjectWithTag(DataManager.playerTag).transform;
+        target = GameObject.FindGameObjectWithTag(DataManager.target).transform;
+    } 
     
 
     void Update(){
@@ -22,12 +34,15 @@ public class EnemyFlying : MonoBehaviour
 
         //set a limit to the enemie view and shooting range
         if(distanceFromPlayer < lineOfSite && distanceFromPlayer > shootingRange){
-            transform.position = Vector3.MoveTowards(this.transform.position, player.position, speed * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(this.transform.position, target.position, speed * Time.deltaTime);
         }
         //preparing to shoot and cooldown for it
         else if(distanceFromPlayer <= shootingRange && fireTime < Time.time){
             Instantiate(bullet, bulletParent.transform.position, Quaternion.identity);
             fireTime = Time.time + fireRate;
+        }
+        else if(timeToDestroy < Time.time){
+            timeToDestroy = timeToDie + Time.time;
         }
 
     }
