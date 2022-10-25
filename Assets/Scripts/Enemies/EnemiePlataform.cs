fbb6ca96bd481cf695d778f19b3f75110609b979
Daniel Bisaggio <danniells.bisaggio@gmail.com>
2022-10-25 20:33:30 -0300

    Moving plataform debbug

--- Assets/Scripts/Enemies/EnemiePlataform.cs
+++ Assets/Scripts/Enemies/EnemiePlataform.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class EnemiePlataform : MonoBehaviour
 {
-
-    [SerializeField] private float amplification;
-    [SerializeField] private float frequency;
-    private Vector3 currentPosition;
-
-    void Awake() => currentPosition = transform.position;
-
-    void Update(){
-        transform.position = new Vector3(currentPosition.x, Mathf.Sin(Time.time * frequency) * amplification + currentPosition.y, currentPosition.z);
+    [SerializeField] private Transform targetA, targetB; 
+    [SerializeField] private float speed = 3f;
+    private bool switching = false;
+    void FixedUpdate()
+    {
+        if (!switching)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetA.position, speed * Time.deltaTime); 
+        }
+        else if (switching)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetB.position, speed * Time.deltaTime); 
+        }
+        if (transform.position == targetA.position)
+        {
+        switching = true;
+        }
+        else if (transform.position == targetB.position)
+        {
+            switching = false;
+        }
     }
-    
 }
