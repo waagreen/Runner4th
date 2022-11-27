65186f862e039998a8183855cfe8f3877a923372
waagreen <silvacostawagner@gmail.com>
2022-11-27 20:17:01 -0300

    character controller updates

--- Assets/Scripts/Movement/PlayerController.cs
+++ Assets/Scripts/Movement/PlayerController.cs
@@ -10,6 +10,8 @@ using System.Threading.Tasks;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] SphereCollider magneticField;
+
     [Header("Jump parameters")]
     [SerializeField] protected LayerMask groundLayers;
     [SerializeField] protected float jumpHeight = 2f, inputGravity = -30f;
@@ -21,7 +23,7 @@ public class PlayerController : MonoBehaviour
     private float? lastGroundedTime;
     private float? jumpButtonPressedTime;
     
-    public bool isGrounded = false; //Tells if player is touching the ground. NEED to be public so other scripts can point to it
+    public bool isGrounded => Physics.CheckSphere(transform.position, .2f, groundLayers);
 
     [Header("Slide parameters")]
     [SerializeField] private float reducedHeight, inputHoldTime = 2f;
@@ -35,15 +37,23 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Animator playerAnim;
     [SerializeField] private TrailController trail;
     [SerializeField] private CharacterAudio cAudio;
+    [SerializeField] private GameObject shield;
+    private Vector3 shieldOriginalScale = Vector3.zero;
+    private Vector3 ShieldOriginalPosition = Vector3.zero;
+    private const float kAnimShildTime = 1.1f;
 
     private UnityEvent deathEvent;
     private UnityEvent shieldEvent;
     private UnityEvent<int> collectEvent;
+
     private Rigidbody rb;
     private PlayerInput inputMap;
-    public Vector3 desiredGravity;
+    private Vector3 desiredGravity;
     private float gravity => desiredGravity.y > 0f ? inputGravity : inputGravity * 3f;
     private bool isDead = false;
+
+    public List<Transform> caughtTransforms = new List<Transform>();
+    private CharacterSheet passiveSkills => DataManager.Events.passiveSkills;
 
     void Start()
     {   
@@ -61,9 +71,9 @@ public class PlayerController : MonoBehaviour
         deathEvent.AddListener(Death);
         collectEvent.AddListener(RemoveCaughtTransform);
         shieldEvent.AddListener(UptadeShield);
-        
         deathEvent = DataManager.Events.OnPlayerDeath;
         deathEvent.AddListener(Death);
+
         rb = GetComponent<Rigidbody>();
 
         originalColliderCenter = mCollider.center;
@@ -90,9 +100,17 @@ public class PlayerController : MonoBehaviour
         Ray playerRay = new Ray(actualRayPosition, Vector3.down);
         if (Physics.Raycast(playerRay, 1.7f, groundLayers.value)) isGrounded = true;
         else isGrounded = false;
-
         trail.ControlEmission(isGrounded);
         if(isGrounded && !cAudio.isPlaying) cAudio.PlaySound(SoundType.running);
+
+        if (caughtTransforms != null && caughtTransforms.Count > 0)
+        {
+            foreach (var t in caughtTransforms)
+            {
+                float coinDist = Vector3.Distance(t.position, transform.position);
+                t.position = Vector3.MoveTowards(t.position, transform.position, 15 * Time.deltaTime);
+            }
+        }
 
         // handle slide cooldown
         slideInputStartTime += Time.deltaTime;
@@ -152,8 +170,14 @@ public class PlayerController : MonoBehaviour
         mCollider.center = new Vector3(0f, 0.3f, 0f); 
         mCollider.height = 0.5f;
 
+        Vector3 newShieldScale = new Vector3(150f, 105f, 150f);
+        Vector3 newShieldPosition = new Vector3(0.2f, 0.3f, 0f);
+
+        shield.transform.DOScale(newShieldScale, kAnimShildTime).SetEase(Ease.OutQuint);
+        shield.transform.DOLocalMove(newShieldPosition, kAnimShildTime).SetEase(Ease.OutQuint);
+
         doingSlide = true;
-        playerAnim.Play("Female Action Pose");
+        playerAnim.Play("Slide_Dalla");
         slideInputStartTime = 0;
         CameraManager.SetNoise(ShakeMode.weak);
         cAudio.PlaySound(SoundType.slide);
@@ -176,8 +200,6 @@ public class PlayerController : MonoBehaviour
             gameObject.SetActive(false);
         }
 
-        isDead = true;
-        gameObject.SetActive(false);
     }
     
     private void UptadeShield() => shield.SetActive(DataManager.GlobalMovement.CurrentShieldCharges > 0);
@@ -194,7 +216,20 @@ public class PlayerController : MonoBehaviour
     {
         if(other.transform.tag == "Obstacle") cAudio.PlaySound(SoundType.collision);   
     }
-
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if(!caughtTransforms.Contains(other.transform) && other.tag == "Collectable")
+        {
+            //Add Transform
+            caughtTransforms.Add(other.transform);
+        }
+    }
+    private void RemoveCaughtTransform(int dontUse)
+    {
+        Transform lastT = caughtTransforms.LastOrDefault();
+        caughtTransforms.Remove(lastT);
+    }
     private void OnDestroy()
     {
         if(inputMap != null)
